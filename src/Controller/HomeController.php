ae4c099712b1742a7c2b0a94f75ac27bb712e756
Auxioma <auxioma@yandex.fr>
2024-02-03 16:24:44 +0100

    API france travail

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,19 +2,22 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Services\PoleEmploiServices;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
     #[IsGranted('ROLE_DWWM', message: 'No access! Get out!', statusCode: 403 )]
-    public function index(): Response
+    public function index(PoleEmploiServices $job): Response
     {
+        $ResultJob = $job->get($query = '');
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'jobs' => json_decode($ResultJob)
         ]);
     }
 }
